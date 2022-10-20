4f6ba3550d82a3f67131add5be22d1934fc49946
Riad Shalahin Leon <105214634+riadleon@users.noreply.github.com>
2022-10-20 21:18:23 +0600

    data loaded from the api

--- src/Pages/Category/Category/Category.js
+++ src/Pages/Category/Category/Category.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 const Category = () => {
+    const news = useLoaderData();
     return (
         <div>
-            <h2>category</h2>
+            <h2>News got: {news.length} </h2>
         </div>
     );
 };
---
+++ src/Pages/Shared/NewsSummaryCard/NewsSummaryCard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NewsSummaryCard = ({news}) => {
+    return (
+        <div>
+            <h2>{news.title}</h2>
+        </div>
+    );
+};
+
+export default NewsSummaryCard;
